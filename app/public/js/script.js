19e1a8176b38dba4058b457939390fbe8665a48c
Kamil <kamil@demotywatory.pl>
2020-06-19 01:09:54 +0200

    script.js - Leaflet instead of OpenLayers
    Also added some layers and switch controll.

--- app/public/js/script.js
+++ app/public/js/script.js
@@ -31,22 +31,71 @@ $(function () {
 		return;
 	}
 
-	var lon = 18.64542;
-	var lat = 54.34766;
-	var zoom = 10;
-
-	new ol.Map({
-		target: 'map-section',
-		layers: [
-			new ol.layer.Tile({
-				source: new ol.source.OSM({
-					url: "https://tile.thunderforest.com/cycle/{z}/{x}/{y}.png?apikey=09c24ee614e44efaa63ca9b4bee54a99"
-				})
-			})
-		],
-		view: new ol.View({
-			center: ol.proj.fromLonLat([lon, lat]),
-			zoom: zoom
-		})
+	const cyclosm = L.tileLayer('https://{s}.tile-cyclosm.openstreetmap.fr/cyclosm/{z}/{x}/{y}.png', {
+		attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors',
+		minZoom: 0,
+		maxZoom: 20,
 	});
+
+	const ocm = L.tileLayer('https://tile.thunderforest.com/cycle/{z}/{x}/{y}.png?apikey=09c24ee614e44efaa63ca9b4bee54a99', {
+		attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors',
+		minZoom: 0,
+		maxZoom: 18,
+	});
+
+	const osm = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+		attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors',
+		minZoom: 0,
+		maxZoom: 19,
+	});
+
+	const googleStreets = L.tileLayer('http://{s}.google.com/vt/lyrs=m&x={x}&y={y}&z={z}',{
+		maxZoom: 20,
+		subdomains:['mt0','mt1','mt2','mt3']
+	});
+
+	const googleHybrid = L.tileLayer('http://{s}.google.com/vt/lyrs=s,h&x={x}&y={y}&z={z}',{
+		maxZoom: 20,
+		subdomains:['mt0','mt1','mt2','mt3']
+	});
+
+	const googleSat = L.tileLayer('http://{s}.google.com/vt/lyrs=s&x={x}&y={y}&z={z}',{
+		maxZoom: 20,
+		subdomains:['mt0','mt1','mt2','mt3']
+	});
+
+	const waymarkedTrails = L.tileLayer('https://tile.waymarkedtrails.org/cycling/{z}/{x}/{y}.png', {
+		attribution: '<a href="https://cycling.waymarkedtrails.org/">https://cycling.waymarkedtrails.org/</a>',
+		minZoom: 0,
+		maxZoom: 18,
+	});
+
+	const cyclosmLite = L.tileLayer('https://{s}.tile-cyclosm.openstreetmap.fr/cyclosm-lite/{z}/{x}/{y}.png', {
+		attribution: 'CyclOSM Lite',
+		minZoom: 11,
+		maxZoom: 20,
+	});
+
+	const map = L.map('map-section', {
+		zoomControl: true,
+		center: [54.34766, 18.64542],
+		zoom: 10,
+		layers: [cyclosm]
+	});
+
+	const baseMaps = {
+		'CyclOSM': cyclosm,
+		'OpenCycleMap': ocm,
+		'OpenStreetMap': osm,
+		'Google Streets': googleStreets,
+		'Google Hybrid': googleHybrid,
+		'Google Satelite': googleSat
+	};
+
+	const overlayMaps = {
+		'Waymarked Trails': waymarkedTrails,
+		'CyclOSM Lite': cyclosmLite
+	};
+
+	L.control.layers(baseMaps, overlayMaps).addTo(map);
 });
